d9e08e6cde8e2f85a072ae9147b36b5006cc3664
Arman Hossain <arman81155@gmail.com>
2021-05-11 20:57:51 +0600

    commit few code

---
+++ ternary-operator.js
@@ -0,0 +1,13 @@
+// var age = 18;
+// if(age >= 18){
+//     console.log("adult");
+// }else{
+//     console.log("not adult")
+// }
+
+var age = 11;
+ var type = (age >= 18) ? "adult" : (age < 10) ? "child" : "teen";
+ console.log(type);
+
+
+ 
